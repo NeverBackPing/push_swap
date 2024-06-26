ea690bfc9cab73efac1f44053957449f385552e4
NeverBackPing <neverbackping@gmail.com>
2024-06-26 14:41:09 +0200

    fix doublons

--- libcft/ft_isdigit.c
+++ libcft/ft_isdigit.c
@@ -6,24 +6,48 @@
 /*   By: sjossain <sjossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 02:46:36 by sjossain          #+#    #+#             */
-/*   Updated: 2024/06/25 19:11:55 by sjossain         ###   ########.fr       */
+/*   Updated: 2024/06/26 14:39:54 by sjossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
+int	check(int first, int second, int digit)
+{
+	if (first == 32)
+	{
+		if (second == 45 || second == 43)
+			return (1);
+	}
+	if ((second == 45 || second == 43) && (digit >= 48 && digit <= 57))
+		return (1);
+	else
+		return (0);
+}
+
 int check_signe(char *str)
 {
 	int		i;
+	//int		check;
 
 	i = 0;
+	//check = 0;
 	while (str[i] != '\0')
 	{
-		if ( str[i]== 45 || str[i] == 43)
+		if ((str[i]== 45 || str[i] == 43))
 		{
+			if (str[i - 1] == 32 || i == 0)
+				NULL;
+			else
+				return (1);
 			i++;
-			if (str[i] >= 48 && str[i] <= 57)
-				return (0);
+			if ((str[i] >= 48 && str[i] <= 57))
+			{
+				if(check(str[i - 2], str[i - 1], str[i]))
+					return (0);
+				else
+					return (1);
+			}
 			else
 				return (1);
 		}
